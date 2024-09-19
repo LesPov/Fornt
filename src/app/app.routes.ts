02acda6c747c3630c00f5f601a8f2c4ed9a4726a
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-09-19 18:11:50 -0500

    modificaciones para ya convinarlo con el back

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+    { path: 'registro', loadComponent: () => import('./components/adminPanel/auth/register/register.component').then(m => m.RegisterComponent) },
+    { path: 'login', loadComponent: () => import('./components/adminPanel/auth/login/login.component').then(m => m.LoginComponent) },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/registro']);
+  }
+}
