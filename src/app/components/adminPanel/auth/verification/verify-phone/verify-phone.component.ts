7375360e0fcfeab9c998d5aa25cf909af75bbef8
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-09-20 13:52:30 -0500

    se creo la verifcaicon para le numero lsito falta mete rla logica

---
+++ src/app/components/adminPanel/auth/verification/verify-phone/verify-phone.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-phone',
+  standalone: true,
+  imports: [],
+  templateUrl: './verify-phone.component.html',
+  styleUrl: './verify-phone.component.css'
+})
+export class VerifyPhoneComponent {
+
+}
